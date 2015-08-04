7072633cb8964b8e520c31ed3439e463e219a708
Lu1zZz <lu1zzz.dsc@gmail.com>
2015-08-03 21:09:36 -0500

    Addon mongoLAB added

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/karmaflow');
+mongoose.connect('mongodb://heroku_3w3p59hh:qp53dldnvpsqrejikp16uediu5@ds029793.mongolab.com:29793/heroku_3w3p59hh');
 
 var db = mongoose.connection;
 
