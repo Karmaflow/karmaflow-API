73fbeae19daf5ac6336056b4e74d1f692be8fdb0
Lu1zZz <lu1zzz.dsc@gmail.com>
2015-08-03 21:20:39 -0500

    MONGOLAB_URI set

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://heroku_3w3p59hh:qp53dldnvpsqrejikp16uediu5@ds029793.mongolab.com:29793/heroku_3w3p59hh');
+//mongoose.connect('mongodb://heroku_3w3p59hh:qp53dldnvpsqrejikp16uediu5@ds029793.mongolab.com:29793/heroku_3w3p59hh');
+mongoose.connect(ENV['S3_KEY']);
 
 var db = mongoose.connection;
 
