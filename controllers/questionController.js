f27f28381219375d82ae628817cb2745df1f0b94
Lu1zZz <lu1zzz.dsc@gmail.com>
2015-08-07 01:22:34 -0500

    Fixed big to insert in questionController

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -10,6 +10,19 @@ var db =  require('../utils/db'),
 var questionController = {}
 
 var getQuestionsbyTag= function(req,res){
+        // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', '*');
+
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+
       var tag = req.params.tag;
         console.log(tag);
         Question.find({'tag':tag},function(err,questions){
